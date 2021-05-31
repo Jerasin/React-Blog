a4b4416ed21adeabff333b4f7fdea98f3f261fba
Phakon Rujireksareekul <2909570-pkmitl205@users.noreply.gitlab.com>
2021-05-31 15:21:54 +0700

    [Add] awesome & fix register

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -11,12 +11,11 @@ export default function Register() {
     user_role: "user",
   });
   const [disabled, setDisabled] = useState(false);
-  const [isError, setisError] = useState(false);
+  const [isError, setisError] = useState(true);
   const [isDuplicate, setisDuplicate] = useState(false);
 
   let history = useHistory();
 
- 
   const modalVai = () => {
     return (
       <div>
@@ -25,13 +24,16 @@ export default function Register() {
           type="button"
           className="btn btn-primary"
           data-bs-toggle="modal"
-          data-bs-target={isError || isDuplicate ? "#errorModal" : "#successModal"}
-          
-          onClick={(e)=>{
-            e.preventDefault()
+          data-bs-target={
+            isError || isDuplicate ? "#errorModal" : "#successModal"
+          }
+          onClick={(e) => {
+            e.preventDefault();
             isRegister();
-            
-            console.log(isError || isDuplicate ? "#errorModal" : "#successModal")
+
+            console.log(
+              isError || isDuplicate ? "#errorModal" : "#successModal"
+            );
           }}
         >
           Register
@@ -111,8 +113,7 @@ export default function Register() {
             </div>
           </div>
         </div>
-    
-          
+
         <div
           className="modal fade"
           id="successModal"
@@ -149,8 +150,6 @@ export default function Register() {
             </div>
           </div>
         </div>
-    
-
       </div>
     );
   };
@@ -201,34 +200,7 @@ export default function Register() {
         className="alert alert-warning d-flex align-items-center"
         role="alert"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" style={{ display: "none" }}>
-          <symbol
-            id="check-circle-fill"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-          </symbol>
-          <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
-          </symbol>
-          <symbol
-            id="exclamation-triangle-fill"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-          </symbol>
-        </svg>
-        <svg
-          className="bi flex-shrink-0 me-2"
-          width={24}
-          height={24}
-          role="img"
-          aria-label="Warning:"
-        >
-          <use xlinkHref="#exclamation-triangle-fill" />
-        </svg>
+        <i className="fa fa-warning" aria-hidden="true" style="color:red;"></i>
         <div>Email is Duplicate</div>
       </div>
     );
@@ -237,7 +209,6 @@ export default function Register() {
   const isRegister = async () => {
     setTimeout(() => {
       setDisabled(false);
-
     }, 1000);
     setDisabled(true);
     let regularEmail = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
